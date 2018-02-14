83bd667f434ed4f3c5570d370ff7337faf63f81a
Daniel Ojeda <211@holbertonschool.com>
2018-02-14 23:19:46 +0000

    new tests

--- tests/test_models/test_user_model.py
+++ tests/test_models/test_user_model.py
@@ -14,78 +14,37 @@ from io import StringIO
 class TestUserClass(unittest.TestCase):
     '''test suite class'''
 
-    def test_type(self):
-        '''tests user inst created'''
-        usr = User()
-        self.assertEqual(type(User()), type(usr))
-
-    def test_email_initial(self):
-        '''test initial value of email attribute'''
-        usr = User()
-        self.assertTrue(type(usr.email) is str)
-
-    def test_password_initial(self):
-        '''test initial value of password attribute'''
-        usr = User()
-        self.assertTrue(type(usr.password) is str)
-
-    def test_first_name_initial(self):
-        '''test initial falue of first_name attribute'''
-        usr = User()
-        self.assertTrue(type(usr.first_name) is str)
-
-    def test_last_name_initial(self):
-        usr = User()
-        self.assertTrue(type(usr.last_name) is str)
-
-    def test_created_at(self):
-        '''created at attribute test'''
-        usr = User()
-        self.assertEqual(datetime, type(usr.created_at))
-
-    def test_updated_at(self):
-        '''updated at attribute test'''
-        usr = User()
-        self.assertEqual(datetime, type(usr.updated_at))
-
-    def test_save_updated(self):
-        '''tests if save is changing updated_at to current datetime'''
-        usr = User()
-        prev = usr.updated_at
-        usr.save()
-        self.assertNotEqual(prev, usr.updated_at)
-
-    def test_updated_at_is_str(self):
-        '''tests if updated_at is a string'''
-        usr = User()
-        usr_dict = usr.to_dict()
-        self.assertEqual(str, type(usr_dict['updated_at']))
-
-    def test_created_at_is_str(self):
-        '''tests if created at is a string'''
-        usr = User()
-        usr_dict = usr.to_dict()
-        self.assertEqual(str, type(usr_dict['created_at']))
-
-    def test_updated_at_to_dict(self):
-        '''tests if updated at is in dictionary'''
-        usr = User()
-        usr_dict = usr.to_dict()
-        self.assertIn('updated_at', usr_dict)
-
-    def test_created_at_to_dict(self):
-        '''tests if created at is in dictionary'''
-        usr = User()
-        usr_dict = usr.to_dict()
-        self.assertIn('created_at', usr_dict)
-
     def test_attributes(self):
         '''checks if attributes exist'''
-        usr = User()
-        self.assertTrue("email" in usr.__dir__())
-        self.assertTrue("password" in usr.__dir__())
-        self.assertTrue("first_name" in usr.__dir__())
-        self.assertTrue("last_name" in usr.__dir__())
+        test1 = User()
+        test2 = User()
+        self.assertTrue(hasattr(test1, "email"))
+        self.assertTrue(hasattr(test2, "password"))
+        self.assertTrue(hasattr(test1, "first_name"))
+        self.assertTrue(hasattr(test2, "last_name"))
+        self.assertTrue(type(test1.email) is str)
+        self.assertTrue(type(test1.first_name) is str)
+        self.assertTrue(type(test2.password) is str)
+        self.assertTrue(type(test2. last_name) is str)
+        self.assertTrue(type(test1.id) is str)
+        self.assertTrue(test1.id != test2.id)
 
+    def test_created_at(self):
+        '''tests created at'''
+        test1 = User()
+        test2 = User()
+        created1 = test1.created_at
+        created2 = test2.created_at
+        self.assertIsNot(created1, created2)
+        self.assertTrue(type(created1) is datetime)
+
+    def test_save_method(self):
+        '''tests save method'''
+        usr = User()
+        test_updated = usr.updated_at
+        usr.save()
+        new_save = usr.updated_at
+        self.assertTrue(test_updated != new_save)
+        
 if __name__ == '__main__':
     unittest.main()
